0011a0c7a36bd354ff5d9d0c65cf38d4db7265c7
Jacob Impson <jacobimpson@gmail.com>
2019-08-27 17:25:38 +0800

    use locale rather than publish_details.locale

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,11 +4,7 @@ const ContentstackAdminBar = require("./src/ContentstackAdminBar").default;
 
 exports.wrapPageElement = ({ element, props }, pluginOptions) => {
   const { CONTENTSTACK_API_KEY } = pluginOptions;
-  const {
-    uid,
-    publish_details: { locale },
-    contentType
-  } = props.pageContext;
+  const { uid, locale, contentType } = props.pageContext;
   const displayBar = uid && locale && contentType && CONTENTSTACK_API_KEY;
   const editEntryUrl = `https://app.contentstack.com/#!/stack/${CONTENTSTACK_API_KEY}/content-type/${contentType}/${locale}/entry/${uid}/edit`;
 
