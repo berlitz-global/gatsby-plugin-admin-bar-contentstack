149f2c88b182224a64fd0b4c8dc57bfa84f4117d
WorkJamesYang <work.jamesyang@gmail.com>
2020-02-18 16:42:41 +1100

    add datakey button

--- src/ContentstackAdminBar.jsx
+++ src/ContentstackAdminBar.jsx
@@ -31,12 +31,53 @@ const EditLink = styled.span`
   font-weight: 400;
   margin-left: 6px;
 `;
-
+const ShowKeys = () =>{
+  var style = document.createElement('style')
+  style.innerHTML = `
+    [data-key] {
+      position: relative;
+    }
+    [data-key] [data-key]::before {
+      background: red;
+    }
+    [data-key] [data-key] [data-key]::before {
+      background: blue;
+    }
+    [data-key] [data-key] [data-key] [data-key]::before {
+      background: pink;
+    }
+    [data-key]::before {
+      top: 0;
+      left: 0;
+      position: absolute;
+      background: green;
+      color: white;
+      font-size: 12px;
+      width: auto;
+      height: auto;
+      white-space: nowrap;
+      z-index: 1;
+    }
+  `
+  document.querySelectorAll('[data-key]')
+    .forEach(myElement =>
+      style.innerHTML = style.innerHTML +
+        `[data-key="${myElement.getAttribute('data-key')}"]::before {
+          content: "${myElement.getAttribute('data-key')}";
+        }`
+    )
+  document.head.appendChild(style);
+}
 const ContentstackAdminBar = ({ editEntryUrl }) => (
-  <Bar href={editEntryUrl} target="_blank">
-    <ContentstackLogo src="https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_120,w_120,f_auto,b_white,q_auto:eco/mzqxvq4tnfrk3n5vva0e" />
-    <EditLink>Edit Entry</EditLink>
-  </Bar>
+  <>
+    <Bar href={editEntryUrl} target="_blank">
+      <ContentstackLogo src="https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_120,w_120,f_auto,b_white,q_auto:eco/mzqxvq4tnfrk3n5vva0e" />
+      <EditLink>Edit Entry</EditLink>
+    </Bar>
+    <Bar href="#" onClick={ShowKeys}>
+      <EditLink>Show Keys</EditLink>
+    </Bar> 
+  </>
 );
 
 export default ContentstackAdminBar;
