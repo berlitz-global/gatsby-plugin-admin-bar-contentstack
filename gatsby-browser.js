4b895e71429588f884527235b634bbf21098cd7e
WorkJamesYang <work.jamesyang@gmail.com>
2020-08-11 18:25:49 +1000

    check opti cookie before render

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,7 +5,11 @@ const ContentstackAdminBar = require("./src/ContentstackAdminBar").default;
 exports.wrapPageElement = ({ element, props }, pluginOptions) => {
   const { CONTENTSTACK_API_KEY } = pluginOptions;
   const { uid, locale, contentType } = props.pageContext;
-  const displayBar = uid && locale && contentType && CONTENTSTACK_API_KEY;
+  const cookieValue = '; ' + document.cookie
+  const cookieParts = cookieValue.split('; optimizely_signed_in=')
+    if (cookieParts.length == 2)
+      return cookieParts.pop().split(';').shift()
+  const displayBar = uid && locale && contentType && CONTENTSTACK_API_KEY && cookieParts;
   const editEntryUrl = `https://app.contentstack.com/#!/stack/${CONTENTSTACK_API_KEY}/content-type/${contentType}/${locale}/entry/${uid}/edit`;
 
   return (
