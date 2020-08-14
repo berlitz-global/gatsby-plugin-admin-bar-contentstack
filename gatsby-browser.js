4806cad6148561f9d654035058e9b723b5b445c4
WorkJamesYang <work.jamesyang@gmail.com>
2020-08-14 17:36:56 +1000

    change cookie name

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,7 +6,7 @@ exports.wrapPageElement = ({ element, props }, pluginOptions) => {
   const { CONTENTSTACK_API_KEY } = pluginOptions;
   const { uid, locale, contentType } = props.pageContext;
   const cookieValue = '; ' + document.cookie
-  const cookieParts = cookieValue.split('; optimizely_signed_in=')
+  const cookieParts = cookieValue.split('; optly_berlitz_test=')
   const displayBar = uid && locale && contentType && CONTENTSTACK_API_KEY && cookieParts.length == 2;
   const editEntryUrl = `https://app.contentstack.com/#!/stack/${CONTENTSTACK_API_KEY}/content-type/${contentType}/${locale}/entry/${uid}/edit`;
 
