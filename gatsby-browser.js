b48414b62526915a3f92b688f4d2575dc7952f4d
WorkJamesYang <work.jamesyang@gmail.com>
2020-08-18 16:14:43 +1000

    change var to const

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,8 +6,8 @@ exports.wrapPageElement = ({ element, props }, pluginOptions) => {
   const { CONTENTSTACK_API_KEY } = pluginOptions;
   const { uid, locale, contentType } = props.pageContext;
   function getCookie(name) {
-    var value = "; " + document.cookie;
-    var parts = value.split("; " + name + "=");
+    const value = "; " + document.cookie;
+    const parts = value.split("; " + name + "=");
     if (parts.length == 2) return parts.pop().split(";").shift();
   }
   const cookieProduction = getCookie('optly_berlitz_test')
